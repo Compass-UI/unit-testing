10b96c2c27ea838087ce3b1feeb330b57d3d273f
David Shams <david.shams@gmail.com>
2018-07-23 21:02:45 -0500

    add intellisense for jasmine and create first test

---
+++ src/calculator.spec.js
@@ -0,0 +1,8 @@
+///<reference path="../lib/jasmine-3.1.0/jasmine.js" />
+
+describe('calculator', function(){
+    it('should add two numbers', function(){
+        expect(add(2,3)).toBe(5)
+    })
+
+})
