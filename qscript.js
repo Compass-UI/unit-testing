af005b524b517eb8bbf5898f63d0766b9521f90e
David Shams <david.shams@gmail.com>
2018-07-24 19:44:12 -0500

    Knokout is Aaaamazing - Better than Angular 2+

--- qscript.js
+++ qscript.js
@@ -2,5 +2,8 @@ $(function(){
     $('button').click(function(){
         vm.fname('John');
         vm.lname('Doe');
-    })
+    });
+    vm.fname.subscribe(function(){
+        console.log('Name changed to: ' + vm.fname());
+    });
 })
